5e4c488fa9ea92fe69753aa2c7de42cfde477b13
Daniel Corson <danl@alum.mit.edu>
2010-07-03 08:06:33 -0400

    assignables-go-right for <, >, <=, >= too (refactored ~fdomig submission)
    Test plan: $a >= 4 -> 4 <= $a

--- src/Transf/AntiPattern.hs
+++ src/Transf/AntiPattern.hs
@@ -21,9 +21,16 @@ exprIsLRVal (ExprRVal (RValLRVal _)) = True
 exprIsLRVal _ = False
 
 exprLRValToRight :: Expr -> Transformed Expr
-exprLRValToRight (ExprBinOp op e1 w e2) =
-  if op `elem` [BEQ, BNE, BID, BNI] && exprIsLRVal e1 && not (exprIsLRVal e2)
-    then pure $ ExprBinOp op e2 w e1
+exprLRValToRight (ExprBinOp op e1 w e2)
+  | op `elem` [BEQ, BNE, BID, BNI] = swapIfGood op
+  | op == BLT = swapIfGood BGT
+  | op == BGT = swapIfGood BLT
+  | op == BLE = swapIfGood BGE
+  | op == BGE = swapIfGood BLE
+  | otherwise = transfNothing
+  where
+  swapIfGood op' = if exprIsLRVal e1 && not (exprIsLRVal e2)
+    then pure $ ExprBinOp op' e2 w e1
     else transfNothing
 exprLRValToRight _ = transfNothing
 
